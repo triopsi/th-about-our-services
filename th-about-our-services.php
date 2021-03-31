ec945c64b84194442c194042ef67cfb051768599
Daniel Rodriguez Baumann <daniel@triopsi.com>
2021-03-31 10:01:05 +0200

    FIX Word break

--- th-about-our-services.php
+++ th-about-our-services.php
@@ -3,7 +3,7 @@
  * Plugin Name: About our services
  * Plugin URI: https://www.wiki.profoxi.de
  * Description: A simple about our services plugin. Create items and copy-paste the shortcode everywhere in your post or site.
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: triopsi
  * Author URI: http://wiki.profoxi.de
  * Text Domain: thaos
@@ -30,7 +30,7 @@
 
 // Definie plugin version.
 if ( ! defined( 'THAOS_VERSION' ) ) {
-	define( 'THAOS_VERSION', '1.1.0' );
+	define( 'THAOS_VERSION', '1.1.1' );
 }
 
 // Loads plugin's text domain.
